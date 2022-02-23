e5ec64e084474fa7ba39f9bcbdccd1cf7a4dafde
andresf5 <31293224+andresf5@users.noreply.github.com>
2022-02-23 12:56:22 -0500

    test cambio de mensaje en la pantalla

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ESTA ES UNA NUEVA VERSION DEL CODIGO DONDE HICE ALGUNOS CAMBIOS.
         </p>
         <a
           className="App-link"
